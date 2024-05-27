d3f79996a00585271d3d22627bad9dfba4955ba7
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-05-27 19:05:07 +0530

    added tour list page

--- src/app/components/home/components/herosection.jsx
+++ src/app/components/home/components/herosection.jsx
@@ -5,6 +5,7 @@ import right2 from '@/app/assets/herosection/img5.png'
 import right3 from '@/app/assets/herosection/img6.png'
 import right4 from '@/app/assets/herosection/img7.png'
 import right1 from '@/app/assets/herosection/img8.png'
+import TourList from './tourList'
 
 
 function Herosection() {
@@ -47,10 +48,11 @@ function Herosection() {
                             
 
                         </div>
-                        <TourList/>
+                        
                     </div>
                 </div>
             </div>
+            <TourList/>
         </>
     )
 }
