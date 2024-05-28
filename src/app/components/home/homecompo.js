73715f48ad4276ba7c8d7399dc65a2a3787c12c4
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-05-28 11:53:36 +0530

    added tour packages component

--- src/app/components/home/homecompo.js
+++ src/app/components/home/homecompo.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Herosection from './components/herosection'
+import TourList from './components/tourList'
+import TourPackages from './components/TourPackages'
 
 function Homepage() {
   return (
     <>
   <Herosection/>
+  <TourList/>
+  <TourPackages />
     </>
   )
 }
