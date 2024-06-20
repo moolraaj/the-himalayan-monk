bb0a7d9340ff453b059b09d1540f354b5ebfea04
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-06-20 15:37:04 +0530

    chnages

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,28 +1,23 @@
-'use client'
-
-// import Link from "next/link"
-// import { leh } from "./assets/images"
+import Link from "next/link"
+import { leh } from "./assets/images"
 
 
-// function Notfound() {
-//   return (
-//     <>
-//       <div className="not_found_banner">
-//         <img src={leh.src} alt={leh.src} />
-//         <h1>page not found <Link href="/">go back to homepage</Link></h1>
-//       </div>
-//       <div className="page_top">
-//         <div className="not_found_outer">
-//           <div className="not_found_inner">
+function Notfound() {
+  return (
+    <>
+      <div className="not_found_banner">
+        <img src={leh.src} alt={leh.src} />
+        <h1>page not found <Link href="/">go back to homepage</Link></h1>
+      </div>
+      <div className="page_top">
+        <div className="not_found_outer">
+          <div className="not_found_inner">
 
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   )
-// }
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
 
-// export default Notfound
-
-
-
+export default Notfound
