37a3b4f5c7c37ed25d7e641bf7a9ffc3905eb409
Rakhi Sharma <sharmarakhi679@gmail.com>
2024-07-10 19:28:33 +0530

    new-changes

--- src/app/destinations/[cityid]/components/locationPackages.jsx
+++ src/app/destinations/[cityid]/components/locationPackages.jsx
@@ -60,8 +60,8 @@ function TourPackages({ id }) {
                 </div>
                 <div className="tour-title">{tour.package_name}</div>
                 <div className="tour-price">
-                  <span className="price">Rs {tour.starting_cost}</span>
-                  <span className="price-details"> <span className="cut-txt"> Rs {tour.discount} </span> <span className="without-cut-txt"> Per Person </span> </span>
+                  <span className="price">Rs {tour.discount} </span>
+                  <span className="price-details"> <span className="cut-txt"> Rs {tour.starting_cost}  </span> <span className="without-cut-txt"> Per Person </span> </span>
                 </div>
                 <button className="book-button">Book A Tour<img src={airplane.src} alt="demo" style={{ width: '28px' }} /></button>
               </div>
