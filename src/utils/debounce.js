56584f544c24ec87990d095e7b357d441b4cdda2
Mr-rakesh-rk975 <145449482+Mr-rakesh-rk975@users.noreply.github.com>
2024-07-29 13:40:58 +0530

    changes

---
+++ src/utils/debounce.js
@@ -0,0 +1,10 @@
+// utils/debounce.js
+export const debounce = (func, delay) => {
+    let debounceTimer;
+    return function (...args) {
+      const context = this;
+      clearTimeout(debounceTimer);
+      debounceTimer = setTimeout(() => func.apply(context, args), delay);
+    };
+  };
+  
